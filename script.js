8a78cba06d926af4e39c56780484b19f7cd8f9ca
EnixWord <116887494+EnixWord@users.noreply.github.com>
2023-01-17 13:51:05 +0300

    fix linters errors

--- script.js
+++ script.js
@@ -1,51 +1,43 @@
 class Books {
-    constructor(title, author) {
-        this.title = title;
-        this.author = author;
-    }
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
 }
 
 const bookArchive = [];
-const form  = document.querySelector('.form');
+const form = document.querySelector('.form');
 const booksDiv = document.querySelector('.books');
-const titleInput = document.getElementsByClassName('book-name');
-const authorInput = document.getElementsByClassName('the-auhtor');
-
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const title = document.querySelector('.book-title').value;
-    const author = document.querySelector('.book-author').value;
-    const book = new Books (title, author);
-
-    bookArchive.push(book);
-    addNewBook(book)
-    
-    localStorage.setItem('bookInfo', JSON.stringify(bookArchive));
-    
-
-})
-
 
 function addNewBook(book) {
-    const bookUnit = document.createElement('li');
-    bookUnit.innerHTML = `
-    <p class="book-name">${book.title}</p>
-    <p class="the-auhtor">${book.author}</p>
-    <button class='removeBook' >Remove</button>
-    `
-    booksDiv.appendChild(bookUnit)
+  const bookUnit = document.createElement('li');
+  bookUnit.innerHTML = `
+      <p class="book-name">${book.title}</p>
+      <p class="the-auhtor">${book.author}</p>
+      <button class='removeBook' >Remove</button>
+      `;
+  booksDiv.appendChild(bookUnit);
 }
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('.book-title').value;
+  const author = document.querySelector('.book-author').value;
+  const book = new Books(title, author);
+
+  bookArchive.push(book);
+  addNewBook(book);
+
+  localStorage.setItem('bookInfo', JSON.stringify(bookArchive));
+});
 
 function removeBook(target) {
-    if (target.classList.contains('removeBook')) {
-        target.parentElement.remove();
-    }
+  if (target.classList.contains('removeBook')) {
+    target.parentElement.remove();
+  }
 }
 
-
 booksDiv.addEventListener('click', (e) => {
-    removeBook(e.target)
-})
+  removeBook(e.target);
+});
